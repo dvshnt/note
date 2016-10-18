77b0b91035385d92c5970ff7eb6a568b13e51063
Ubuntu <ubuntu@ip-172-31-24-185.us-west-2.compute.internal>
2016-10-18 13:33:51 +0000

    Settings and WSGI samples added.

---
+++ app/server/wsgi/sample.wsgi.py
@@ -0,0 +1,20 @@
+"""
+Sample WSGI config for note project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+path = 'PATH TO DJANGO APP ROOT'
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "path.to.settings.file")
+
+application = get_wsgi_application()
